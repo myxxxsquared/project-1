03a17154a55995e9540655e6991ebf421a0cdb95
ruanjiaqiang <randyruan96@gmail.com>
2018-02-07 17:52:09 +0800

    load preprocessing

--- model/Models.py
+++ model/Models.py
@@ -164,7 +164,7 @@ class  model(object):
                               tr, 1)
         self.image_summary_op = tf.summary.merge([i1, i2, i3, i4, i5])
 
-    def get_training_func(self):
+    def get_training_func(self, initializer):
         def fn(features):
             return self._build_network(features)
         return fn
