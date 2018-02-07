deb7992a7c90fd684f976b9c3329642a788259f6
ruanjiaqiang <randyruan96@gmail.com>
2018-02-07 12:45:57 +0800

    load preprocessing

--- data/utils.py
+++ data/utils.py
@@ -731,7 +731,7 @@ if __name__ == '__main__':
     PKL_DIR = '/home/rjq/data_cleaned/pkl/'
     import pickle
 
-    for i in range(10000):
+    for i in range(800000):
         res = pickle.load(open(PKL_DIR+'synthtext_chars/'+str(i)+'.bin', 'rb'))
         # print(res['img_name'],
         #       res['contour'],
