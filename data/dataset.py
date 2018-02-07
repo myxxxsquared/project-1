2732c22a8491b8d3555140105260975f64aca553
ruanjiaqiang <randyruan96@gmail.com>
2018-02-07 11:43:30 +0800

    load preprocessing

--- data/dataset.py
+++ data/dataset.py
@@ -28,6 +28,12 @@ def wrapper(index):
     file = PKL_DIR + 'totaltext_train/' + str(index) + '.bin'
     img_name, img, maps = data_label(data_aug(load_file(file), augment_rate=100))
     [TR, TCL, radius, cos_theta, sin_theta] = maps
+    img = tf.cast(img, tf.float32)
+    TR = tf.cast(TR, tf.float32)
+    TCL = tf.cast(TCL, tf.float32)
+    radius = tf.cast(radius, tf.float32)
+    cos_theta = tf.cast(cos_theta, tf.float32)
+    sin_theta = tf.cast(sin_theta, tf.float32)
     return img, TR, TCL, radius, cos_theta, sin_theta
 
 
