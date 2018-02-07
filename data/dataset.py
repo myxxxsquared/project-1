916e76d1726f58d9bacbf18091ae32447c75e8b3
JiaqiangRuan <randyruan96@gmail.com>
2018-02-08 02:05:03 +0800

    framework

--- data/dataset.py
+++ data/dataset.py
@@ -59,7 +59,7 @@ def get_train_input(params):
     syn_dataset = syn_dataset.map(
         lambda index: tuple(tf.py_func(
             syn_wrapper, [index], [tf.float32,tf.float32,tf.float32,tf.float32,tf.float32,tf.float32])),
-        num_threads=64).prefetch(BUFFER_SIZE)
+        num_parallel_calls=40).prefetch(BUFFER_SIZE)
 
     syn_dataset = syn_dataset.batch(32)
 
