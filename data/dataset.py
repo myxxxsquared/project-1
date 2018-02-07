57b33beda852f9857ebd0cc857f8889b611ded40
ruanjiaqiang <randyruan96@gmail.com>
2018-02-07 15:59:02 +0800

    load preprocessing

--- data/dataset.py
+++ data/dataset.py
@@ -22,29 +22,67 @@ def data_label(ins):
     return labelling._data_labeling(ins['img_name'],ins['img'],
                                     ins['contour'],ins['is_text_cnts'],
                                     ins['left_top'],ins['right_bottom'],
-                                    None)
+                                    ins.get('chars',None))
 
 
-def wrapper(index):
+def syn_wrapper(index):
+    file = PKL_DIR + 'synthtext_char/' + str(index) + '.bin'
+    img_name, img, maps = data_label(data_aug(load_file(file), augment_rate=100))
+    [TR, TCL, radius, cos_theta, sin_theta] = maps
+    img = np.array(img, np.float32)
+    TR = np.array(TR, np.float32)
+    TCL = np.array(TCL, np.float32)
+    radius = np.array(radius, np.float32)
+    cos_theta = tf.cast(cos_theta, tf.float32)
+    sin_theta = tf.cast(sin_theta, tf.float32)
+    return img, TR, TCL, radius, cos_theta, sin_theta
+
+def total_train_wrapper(index):
     file = PKL_DIR + 'totaltext_train/' + str(index) + '.bin'
     img_name, img, maps = data_label(data_aug(load_file(file), augment_rate=100))
     [TR, TCL, radius, cos_theta, sin_theta] = maps
-    # img = tf.cast(img, tf.float32)
-    # TR = tf.cast(TR, tf.float32)
-    # TCL = tf.cast(TCL, tf.float32)
+    img = np.array(img, np.float32)
+    TR = np.array(TR, np.float32)
+    TCL = np.array(TCL, np.float32)
     radius = np.array(radius, np.float32)
-    # cos_theta = tf.cast(cos_theta, tf.float32)
-    # sin_theta = tf.cast(sin_theta, tf.float32)
-    return radius
+    cos_theta = tf.cast(cos_theta, tf.float32)
+    sin_theta = tf.cast(sin_theta, tf.float32)
+    return img, TR, TCL, radius, cos_theta, sin_theta
+
+def total_test_wrapper(index):
+    file = PKL_DIR + 'totaltext_test/' + str(index) + '.bin'
+    img_name, img, maps = data_label(data_aug(load_file(file), augment_rate=100))
+    [TR, TCL, radius, cos_theta, sin_theta] = maps
+    img = np.array(img, np.float32)
+    TR = np.array(TR, np.float32)
+    TCL = np.array(TCL, np.float32)
+    radius = np.array(radius, np.float32)
+    cos_theta = tf.cast(cos_theta, tf.float32)
+    sin_theta = tf.cast(sin_theta, tf.float32)
+    return img, TR, TCL, radius, cos_theta, sin_theta
 
 
-def get_train_input():
-    dataset = tf.data.Dataset.range(1000)
-    dataset = dataset.map(
+def get_train_input(params):
+    syn_dataset = tf.data.Dataset.range(858749+1).repeat(params.pretrain_num)
+
+    syn_dataset = syn_dataset.map(
         lambda index: tuple(tf.py_func(
-            wrapper, [index], [tf.float32])))
-    iterator = dataset.make_one_shot_iterator()
-    dataset = dataset.batch(32)
+            syn_wrapper, [index], [tf.float32,tf.float32,tf.float32,tf.float32,tf.float32,tf.float32])))
+
+    total_train_dataset = tf.data.Dataset.range(858749+1).repeat(params.pretrain_num)
+    total_train_dataset = total_train_dataset.map(
+        lambda index: tuple(tf.py_func(
+            total_train_wrapper, [index], [tf.float32,tf.float32,tf.float32,tf.float32,tf.float32,tf.float32])))
+
+    total_test_dataset = tf.data.Dataset.range(858749+1).repeat(params.pretrain_num)
+    total_test_dataset = total_test_dataset.map(
+        lambda index: tuple(tf.py_func(
+            total_test_wrapper, [index], [tf.float32,tf.float32,tf.float32,tf.float32,tf.float32,tf.float32])))
+
+
+    syn_dataset = syn_dataset.batch(32)
+    iterator = syn_dataset.make_one_shot_iterator()
+
     features = iterator.get_next()
     return features
 
