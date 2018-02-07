3bf2611377cbefe4d56f056dbedc95cb74ce7d63
ruanjiaqiang <randyruan96@gmail.com>
2018-02-07 16:29:55 +0800

    load preprocessing

--- data/dataset.py
+++ data/dataset.py
@@ -44,7 +44,7 @@ def get_train_input(params):
 
     syn_dataset = syn_dataset.map(
         lambda index: tuple(tf.py_func(
-            syn_wrapper, [index], tf.float32)))
+            syn_wrapper, [index], [tf.float32])))
 
     # total_train_dataset = tf.data.Dataset.range(858749+1).repeat(params.pretrain_num)
     # total_train_dataset = total_train_dataset.map(
