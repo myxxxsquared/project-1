3c1c4ff99fd68689c17ffc879d0f9117b0940352
ruanjiaqiang <randyruan96@gmail.com>
2018-02-08 17:18:50 +0800

    load preprocessing

--- bin/train.py
+++ bin/train.py
@@ -230,16 +230,16 @@ def main(args):
 
 
         # Create session, do not use default CheckpointSaverHook
-            with tf.train.MonitoredTrainingSession(
-                    checkpoint_dir=params.output, hooks=train_hooks,
-                    save_checkpoint_secs=None, config=config) as sess:
-                coord = tf.train.Coordinator()
-                threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-                while not sess.should_stop():
-                    # Bypass hook calls
-                    sess.run(train_op)
-                coord.request_stop()
-                coord.join(threads)
+        with tf.train.MonitoredTrainingSession(
+                checkpoint_dir=params.output, hooks=train_hooks,
+                save_checkpoint_secs=None, config=config) as sess:
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            while not sess.should_stop():
+                # Bypass hook calls
+                sess.run(train_op)
+            coord.request_stop()
+            coord.join(threads)
 
 if __name__ == "__main__":
     main(parse_args())
