f8d3576e5b157abb77526d2ebb8aab1de21ae11f
ruanjiaqiang <randyruan96@gmail.com>
2018-02-08 15:32:42 +0800

    load preprocessing

--- data/data_augementation.py
+++ data/data_augementation.py
@@ -4,40 +4,17 @@ import numpy as np
 from imgaug import augmenters as iaa
 import imgaug as ia
 from random import shuffle, randint, choice, random
-import cv2
 import copy
 
+def _get_seq(self, affine=False):
+    if affine:
+        affine = [
+            iaa.Affine(rotate=360*np.random.rand()-180),
+            iaa.Affine(shear=80*np.sin(np.random.rand()*np.pi/2)-40),
+            iaa.Affine(scale={"x": 0.4*np.random.rand()+0.8, "y": 0.4*np.random.rand()+0.8})
+        ]
+        return iaa.Sequential([choice(affine)])#
 
-"""
-all data_augmentation defined below should take input as :
-{'img_name':str,   original_name
-    'img':np.uint8,
-    'contour':List[np.array(the contour of each text instance), (n,1,2)], ---> np.array(num_TI, num_point, 1, 2),
-    'is_text_cnts': bool, true for cnts of boxes,
-                        false for cnts of char}
-
-synthetext: no augment, char, False;
-others: augment, box
-
-while returning the same format.
-
-valuable data augmentation types:
-    **crop, pad, flip,
-    invert/add(overall/channel), add to hue and saturation, multiply,
-    gaussian blur, average blur, median blur, bilateral blur,
-    sharpen, emboss, edge detect,
-    noise: guassian(overall/channel), dropout(pixel/channel, coarse,channel), salt&pepper
-    norm: contrast
-    gray scale
-    **affine transformation#i should implement it myself, maybe
-
-it takes around 0.25~0.3s to generate one augmented image on average.
-50% are processed with affine transformation
-
-"""
-
-
-def _get_seq(affine=False):
     add_augmentation_list = [
         iaa.Add((-30, 30), per_channel=True),
         iaa.Add((-30, 30), per_channel=False),
@@ -50,7 +27,7 @@ def _get_seq(affine=False):
         iaa.Multiply((0.8, 1.2), per_channel=False),
         iaa.MultiplyElementwise((0.8, 1.2), per_channel=True),
         iaa.MultiplyElementwise((0.8, 1.2), per_channel=False)
-    ]
+     ]
 
     blur_augmentation_list = [
         iaa.GaussianBlur((2, 3)),
@@ -76,14 +53,6 @@ def _get_seq(affine=False):
         iaa.Grayscale((0, 1))
     ]
 
-    if affine:
-        affine = [
-            iaa.Affine(rotate=360*np.random.rand()-180),
-            iaa.Affine(shear=80*np.sin(np.random.rand()*np.pi/2)-40),
-            iaa.Affine(scale={"x": 0.4*np.random.rand()+0.8, "y": 0.4*np.random.rand()+0.8})
-        ]
-        return iaa.Sequential([choice(affine)])#
-
     shuffle(add_augmentation_list)
     add_augmentation_list = add_augmentation_list[:randint(0, 1)]
     shuffle(other_augmentation_list)
@@ -100,7 +69,6 @@ def _get_seq(affine=False):
 
     return iaa.Sequential(final_list[:2])
 
-
 def _key_points(image_shape, point_list):
     """
     feed cnt and return ia.KeypointsOnImage object
@@ -113,7 +81,6 @@ def _key_points(image_shape, point_list):
         keypoint_list.append(ia.Keypoint(x=point_list[i, 0, 0], y=point_list[i, 0, 1]))
     return ia.KeypointsOnImage(keypoint_list,
                                shape=ia.quokka(size=image_shape[:2]))
-
 
 def _resize(input_data):
     """
@@ -137,10 +104,82 @@ def _resize(input_data):
                 iaa.Scale({'height': 512, 'width': "keep-aspect-ratio"}, 'cubic')
             ])
             input_data['img'] = seq.augment_image(input_data['img'])
-
-    input_data['contour'] = [np.cast['int32'](contour*rate) for contour in input_data['contour']]
+    if input_data['is_text_cnts']:
+        input_data['contour'] = [np.cast['int32'](contour * rate) for contour in input_data['contour']]
+    else:
+        input_data['contour'] = [[np.cast['int32'](contour * rate) for contour in contours] for contours in
+                                 input_data['contour']]
     input_data['center_point'] = [(np.cast['int32'](point[0] * rate),
                                    np.cast['int32'](point[1] * rate)) for point in input_data['center_point']]
+    return input_data
+
+def _resize_to_32x(input_data):
+    """
+    resize image to multilpes of 32
+    :param input_data:
+    :return:
+    return scaled img
+    """
+    rate = 1
+
+    p=0
+
+    while True:
+        if (input_data['img'].shape[0]+p)%32==0: row=input_data['img'].shape[0]+p;break
+        if (input_data['img'].shape[0] - p) % 32 == 0: row = input_data['img'].shape[0] - p;break
+        p+=1
+    p=0
+    while True:
+        if (input_data['img'].shape[1]+p)%32==0: col=input_data['img'].shape[1]+p;break
+        if (input_data['img'].shape[1] - p) % 32 == 0: col = input_data['img'].shape[1] - p;break
+        p+=1
+
+    if input_data['is_text_cnts']:
+        input_data['contour'] = [np.cast['int32'](np.stack([contour[:,:,0] * row/input_data['img'].shape[0],
+                                                            contour[:,:,1] * col/input_data['img'].shape[1]],
+                                                           axis=-1)) for contour in input_data['contour']]
+    else:
+        input_data['contour'] = [[np.cast['int32'](np.stack([contour[:,:,0] * row/input_data['img'].shape[0],
+                                                            contour[:,:,1] * col/input_data['img'].shape[1]],
+                                                           axis=-1))
+                                  for contour in contours] for contours in input_data['contour']]
+    input_data['center_point'] = [(np.cast['int32'](point[0] * row/input_data['img'].shape[0]),
+                                   np.cast['int32'](point[1] * col/input_data['img'].shape[1]))
+                                  for point in input_data['center_point']]
+
+    seq = iaa.Sequential([
+        iaa.Scale({'height': row, 'width': col}, 'cubic')
+    ])
+    input_data['img'] = seq.augment_image(input_data['img'])
+
+    return input_data
+
+def _resize_to_512(input_data):
+    """
+    resize image to 512*512
+    :param input_data:
+    :return:
+    return scaled img
+    """
+
+    if input_data['is_text_cnts']:
+        input_data['contour'] = [np.cast['int32'](np.stack([contour[:,:,0] * 512/input_data['img'].shape[0],
+                                                            contour[:,:,1] * 512/input_data['img'].shape[1]],
+                                                           axis=-1)) for contour in input_data['contour']]
+    else:
+        input_data['contour'] = [[np.cast['int32'](np.stack([contour[:,:,0] * 512/input_data['img'].shape[0],
+                                                            contour[:,:,1] * 512/input_data['img'].shape[1]],
+                                                           axis=-1))
+                                  for contour in contours] for contours in input_data['contour']]
+    input_data['center_point'] = [(np.cast['int32'](point[0] * 512/input_data['img'].shape[0]),
+                                   np.cast['int32'](point[1] * 512/input_data['img'].shape[1]))
+                                  for point in input_data['center_point']]
+
+    seq = iaa.Sequential([
+        iaa.Scale({'height': 512, 'width': 512}, 'cubic')
+    ])
+    input_data['img'] = seq.augment_image(input_data['img'])
+
     return input_data
 
 def _pad(input_data):
@@ -158,9 +197,17 @@ def _pad(input_data):
 
     input_data['img'] = np.pad(input_data['img'], ((up, down), (left, right), (0, 0)), mode='constant')
 
-    input_data['contour'] = list(
+    if input_data['is_text_cnts']:
+        input_data['contour'] = list(
         map(lambda x: np.stack([x[:, :, 0] + up, x[:, :, 1] + left], axis=-1),  # x: np.array(n,1,2)
             input_data['contour']))
+    else:
+        input_data['contour'][0] = list(
+            map(lambda x: np.stack([x[:, :, 0] + up, x[:, :, 1] + left], axis=-1),  # x: np.array(n,1,2)
+                input_data['contour'][0]))
+        input_data['contour'][1] = list(
+            map(lambda x: np.stack([x[:, :, 0] + up, x[:, :, 1] + left], axis=-1),  # x: np.array(n,1,2)
+                input_data['contour'][1]))
 
     input_data['center_point'] = list(
         map(lambda x: (x[0]+up, x[1]+left),
@@ -202,14 +249,25 @@ def _affine_transformation(inputs, trans_rate=0.5):
     if random() > 1-trans_rate:
         transformer = _get_seq(affine=True)
         input_data['img'] = transformer.augment_image(input_data['img'])
-        for p, cnt in enumerate(input_data['contour']):
-            input_data['contour'][p] = transformer.augment_keypoints([
-                _key_points(image_shape=input_data['img'].shape, point_list=cnt)
-            ])[0]
-            input_data['contour'][p] = [np.array([[int(keypoints.y),
-                                                   int(keypoints.x)]])[:, ::-1]
-                                        for keypoints in input_data['contour'][p].keypoints]
-            input_data['contour'][p] = np.stack(input_data['contour'][p], axis=0)
+        if input_data['is_text_cnts']:
+            for p, cnt in enumerate(input_data['contour']):
+                input_data['contour'][p] = transformer.augment_keypoints([
+                    _key_points(image_shape=input_data['img'].shape, point_list=cnt)
+                ])[0]
+                input_data['contour'][p] = [np.array([[int(keypoints.y),
+                                                       int(keypoints.x)]])[:, ::-1]
+                                            for keypoints in input_data['contour'][p].keypoints]
+                input_data['contour'][p] = np.stack(input_data['contour'][p], axis=0)
+        else:
+            for t in range(2):
+                for p, cnt in enumerate(input_data['contour'][t]):
+                    input_data['contour'][t][p] = transformer.augment_keypoints([
+                        _key_points(image_shape=input_data['img'].shape, point_list=cnt)
+                    ])[0]
+                    input_data['contour'][t][p] = [np.array([[int(keypoints.y),
+                                                           int(keypoints.x)]])[:, ::-1]
+                                                for keypoints in input_data['contour'][t][p].keypoints]
+                    input_data['contour'][t][p] = np.stack(input_data['contour'][t][p], axis=0)
 
         input_data['center_point'][0] = \
             transformer.augment_keypoints([
@@ -247,7 +305,7 @@ def _crop_flip_pad(input_data):
     y = center1[1]+int(p*(center2[1]-center1[1]))
     return x, y
 
-def augment(input_, augment_rate=100, trans_rate=0.5):
+def augment(input_, augment_rate=100, trans_rate=0.5,test_mode=False,real_test=False):
     """
 
     :param input_data:
@@ -255,7 +313,11 @@ def augment(input_, augment_rate=100, trans_rate=0.5):
             'img':np.uint8,
             'contour':List[the contour of each text instance],
             'type': 'char' or 'tl',
-            'is_text_cnts':if this is synthetext or not}
+            'is_text_cnts':if this is synthetext or not,
+            'char'}
+            contour:
+            if is_text_cnts is True: list(ndarray), ndarray: dtype np.float32, shape [n, 1, 2], order(col, row)
+            if is_text_cnts is False: list(list(ndarray), list(ndarray)), for [char_cnts, text_cnts]
     :param augment_rate
     :param trans_rate
     :return:
@@ -268,7 +330,16 @@ def augment(input_, augment_rate=100, trans_rate=0.5):
              'right_bottom': tuple (x, y), x is row, y is col}
     """
     input_data=copy.deepcopy(input_)
-    input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
+    if (not input_data['is_text_cnts']):
+        if len(input_data['contour'][0][0].shape)<3:
+            for i in range(2):
+                input_data['contour'][i] = [np.stack([cnt],axis=1) for cnt in input_data['contour'][i]]
+                input_data['contour'][i] = [cnt[:, :, ::-1] for cnt in input_data['contour'][i]]
+    else:
+        if len(input_data['contour'][0].shape) < 3:
+            input_data['contour'] = [np.stack([cnt], axis=1) for cnt in input_data['contour']]
+        input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
+
     if input_data['img'].shape[0] < input_data['img'].shape[1]:
         input_data['center_point'] = [(input_data['img'].shape[0] // 2, input_data['img'].shape[0] // 2),
                                   (input_data['img'].shape[0] // 2, input_data['img'].shape[1] - input_data['img'].shape[0] // 2)]
@@ -276,11 +347,36 @@ def augment(input_, augment_rate=100, trans_rate=0.5):
         input_data['center_point'] = [(input_data['img'].shape[1] // 2, input_data['img'].shape[1] // 2),
                                   (input_data['img'].shape[0] - input_data['img'].shape[1] // 2, input_data['img'].shape[1] // 2)]
 
+    if real_test:
+        input_data = _resize_to_32x(input_data)
+        input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
+        return {
+            **input_data,
+            'left_top': (0, 0),
+            'right_bottom': (input_data['img'].shape[0], input_data['img'].shape[1])
+        }
+
+    if test_mode:
+        input_data=_resize_to_512(input_data)
+        input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
+        return {
+            **input_data,
+            'left_top': (0, 0),
+            'right_bottom': (input_data['img'].shape[0], input_data['img'].shape[1])
+        }
+
     input_data = _resize(input_data)
+
+
+
 
     if (not input_data['is_text_cnts']) or (random() < (1.0 / augment_rate)):
         center_point = _crop_flip_pad(input_data)
-        input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
+        if (not input_data['is_text_cnts']):
+            for i in range(2):
+                input_data['contour'][i] = [cnt[:, :, ::-1] for cnt in input_data['contour'][i]]
+        else:
+            input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
         x_top=max(0,center_point[0] - 256)
         y_left=max(0,center_point[1] - 256)
         return {
@@ -301,38 +397,4 @@ def augment(input_, augment_rate=100, trans_rate=0.5):
                 'left_top': (x_top, y_left),
                 'right_bottom': (x_top+512, y_left+512)
         }
-
-
-    # @staticmethod
-    # def demo(input_data_,show=True):
-    #     """
-    #     show the image and the key points
-    #     :param input_data:
-    #     :param crop_point_starting:
-    #     :return:
-    #     """
-    #     input_data = copy.deepcopy(input_data_)
-    #     input_data['contour'] = [cnt[:, :, ::-1] for cnt in input_data['contour']]
-    #
-    #     for i in range(len(input_data['contour'])):
-    #         for point in range(input_data['contour'][i].shape[0]):
-    #             input_data['img'][input_data['contour'][i][point, 0, 0] - 5:input_data['contour'][i][point, 0, 0] + 5,
-    #                 input_data['contour'][i][point, 0, 1] - 5:input_data['contour'][i][point, 0, 1] + 5, :] \
-    #                 = (0, 255, 255)
-    #
-    #     input_data['img'][input_data['center_point'][0][0]-5:input_data['center_point'][0][0]+5,
-    #         input_data['center_point'][0][1]-5:input_data['center_point'][0][1]+5, :] = (255, 255, 0)
-    #     input_data['img'][input_data['center_point'][1][0]-5:input_data['center_point'][1][0]+5,
-    #         input_data['center_point'][1][1]-5:input_data['center_point'][1][1]+5, :] = (255, 255, 0)
-    #     input_data['img'][input_data['left_top'][0]:input_data['right_bottom'][0],
-    #         input_data['left_top'][1]:input_data['right_bottom'][1],:]+=30
-    #     if show:
-    #         cv2.imshow('show', input_data['img'])  # np.transpose(img, axes=[1, 0, 2]))
-    #     else:
-    #         cv2.imshow('show', input_data['img'][input_data['left_top'][0]:input_data['right_bottom'][0],
-    #         input_data['left_top'][1]:input_data['right_bottom'][1],:])  # np.transpose(img, axes=[1, 0, 2]))
-    #     cv2.waitKey(1)
-
-
-
 
