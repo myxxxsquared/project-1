4fc04fade69ed894167b5f832fd6a273226e3dab
ruanjiaqiang <randyruan96@gmail.com>
2018-02-23 17:00:11 +0800

    test mp

--- data/dataset.py
+++ data/dataset.py
@@ -30,31 +30,32 @@ def loading_data(file, test_mode=False, real_test=False):
     return _data_label(_data_aug(_load_file(file), augment_rate=100, test_mode=test_mode, real_test=real_test))
 
 
+q = mp.Queue()
+
+def enqueue(q, start, end):
+    for i in range(start, end):
+        # q.put({'input_img': np.ones((12, 512,512, 3)).astype(np.float32),
+        #        'Labels': np.ones((12, 512,512,5)).astype(np.float32)})
+        q.put(i)
+
+starts = [0, 100, 200]
+ends = [100, 200, 300]
+
+jobs = []
+for i in range(3):
+    jobs.append(mp.Process(target=enqueue, args=(q, starts[i], ends[i])))
+for i in range(3):
+    jobs[i].start()
+
+
+def generator(q):
+    while True:
+        # with tf.device('/cpu:0'):
+        #     features = {'input_img': tf.convert_to_tensor(np.ones((12, 512,512, 3)).astype(np.float32)),
+        #                 'Labels': tf.convert_to_tensor(np.ones((12, 512,512,5)).astype(np.float32))}
+        yield q.get()
+
 def get_train_input(params):
-    q = mp.Queue()
-
-    def enqueue(q, start, end):
-        for i in range(start, end):
-            # q.put({'input_img': np.ones((12, 512,512, 3)).astype(np.float32),
-            #        'Labels': np.ones((12, 512,512,5)).astype(np.float32)})
-            q.put(i)
-
-    starts = [0, 100, 200]
-    ends = [100, 200, 300]
-
-    jobs = []
-    for i in range(3):
-        jobs.append(mp.Process(target=enqueue, args=(q, starts[i], ends[i])))
-    for i in range(3):
-        jobs[i].start()
-
-    def generator(q):
-        while True:
-            # with tf.device('/cpu:0'):
-            #     features = {'input_img': tf.convert_to_tensor(np.ones((12, 512,512, 3)).astype(np.float32)),
-            #                 'Labels': tf.convert_to_tensor(np.ones((12, 512,512,5)).astype(np.float32))}
-            yield q.get()
-
     return generator(q).__next__()
 
 if __name__ == '__main__':
