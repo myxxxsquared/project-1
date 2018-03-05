9721c5a9889c5695d3c3ad09eab77c2153ca0bad
JiaqiangRuan <randyruan96@gmail.com>
2018-03-05 22:34:00 +0800

    add pixel labeling

--- model/pixellink.py
+++ model/pixellink.py
@@ -155,7 +155,7 @@ class PixelLinkNetwork:
             weighted_loss = cross_entropy[0] * weights
             pos_loss = pos_region * weighted_loss
             neg_loss = neg_region * weighted_loss
-            print(tf.nn.top_k(neg_loss, k=k).shape)
+            # print(tf.nn.top_k(neg_loss, k=k).shape)
             print(tf.nn.top_k(neg_loss, k=k).dtype)
             T_loss = (tf.reduce_sum(pos_loss) +
                       tf.reduce_sum(tf.nn.top_k(neg_loss, k=k))) / (1 + r)
